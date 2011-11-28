c1cc9f80c1bd031ed2be09c722d86b9757e3aac9
Daniel Kodeih <d_kod@yahoo.com>
2011-11-28 15:14:53 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab8::Application.config.secret_token = 'e1b8a952e2d52368c01e7bcea10c6bcd206ed7b1c7c7a98a5260cba852498a3b9dbb98cdc75227956e94fe1ce978c6fe9897199329226e844cbd60f835141cb6'
