1c1368516f002bebeb8ed28203576a8be4ae5204
Daniel Kodeih <d_kod@yahoo.com>
2011-12-10 18:07:47 -0600

    Sign Up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,16 @@ class UsersController < ApplicationController
   end
 
   def new
+     @user = User.new
      @title = "Signup"
+  end
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+       redirect_to @user
+    else
+      @title = "Sign up"
+      render 'new'
+    end
   end
 end
